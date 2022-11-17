c2e8e1b78af4788d1455e41d20abc026820b2b41
Arch1ve <afanasiy.m2002@gmail.com>
2022-11-17 17:42:18 +0300

    Added logo link

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -5,15 +5,23 @@ import Tabs from '../tabs/tabs'
 
 import '../../scss/style.scss'
 import styles from './app.module.scss'
+import logo from './img/Logo.svg'
 
 const App = () => {
   return (
-    <div className={styles.wrapper}>
-      <Filters />
-      <div className={styles.content}>
-        <Tabs />
+    <React.Fragment>
+      <header className={styles.header}>
+        <a href="#" className={styles.logo}>
+          <img src={logo} />
+        </a>
+      </header>
+      <div className={styles.wrapper}>
+        <Filters />
+        <main className={styles.main}>
+          <Tabs />
+        </main>
       </div>
-    </div>
+    </React.Fragment>
   )
 }
 
